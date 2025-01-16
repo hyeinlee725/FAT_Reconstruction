34b7be987c48f1cac755ccf6ccf082269f4803f9
Hye-In Lee <59090549+hyeinlee725@users.noreply.github.com>
2025-01-17 08:56:11 +0900

    Update and rename fat2.py to main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+ 
+from fat32 import *
+ 
+import pprint
+ 
+fat32 = Fat32("../data/fat32.mdf")
+fs = fat32.build_filesystem()
+ 
+pprint.pp(fs.nodes.keys())
+leaf = fs["/DIR1/leaf.jpg"]
+leaf.export_to("/Users/yielding/Desktop/leaf.jpg")
+port = fs["/DIR1/PORT.JPG"]
+port.export_to("/Users/yielding/Desktop/port.jpg")
+port = fs["/DIR1/thumb_nail.py"]
+port.export_to("/Users/yielding/Desktop/thumb_nail.py")
+port = fs["/?iger.jpg"]
+port.export_to("/Users/yielding/Desktop/tiger.jpg")
