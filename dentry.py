346ef298337d4c2cfb8574dac23b8fe3fc5701ae
Hye-In Lee <59090549+hyeinlee725@users.noreply.github.com>
2025-01-17 16:47:19 +0900

    Update dentry.py

--- dentry.py
+++ dentry.py
@@ -8,11 +8,14 @@ class Dentry:
         self.extension = self.data.get_ascii(3).strip()
         self.attr = self.data.get_uint1()
         self.data.offset = 0x14
-        self.first_cluster_high = self.data.get_uint2_le()
+        self.cluster_hi = self.data.get_uint2_le()
         self.data.offset = 0x1A
-        self.first_cluster_low = self.data.get_uint2_le()
+        self.cluster_lo = self.data.get_uint2_le()
         self.file_size = self.data.get_uint4_le()
 
+    def cluster_no(self):
+        return hex((self.cluster_lo << 8) | self.cluster_hi)
+        
     def is_vol(self):
         return self.attr & 0x08 != 0
 
@@ -24,10 +27,10 @@ class Dentry:
 
     def is_lfn(self):
         return self.attr & 0x0F == 0x0F
-
-    def cluster_no(self):
-        return hex((self.first_cluster_high << 16) | self.first_cluster_low)
-
+    
+    def dentries_class(dentries, key_func, init_val):
+        return reduce(lambda acc, entry: acc + key_func(entry), dentries, init_val)
+        pass
     def __str__(self):
         return (f"Name: {self.name}.{self.extension}\n"
                 f"Attribute: {hex(self.attr)}\n"
@@ -36,12 +39,13 @@ class Dentry:
                 f"Directory: {self.is_dir()}\n"
                 f"File: {self.is_file()}\n"
                 f"Volume: {self.is_vol()}\n"
-                f"LFN: {self.is_lfn()}")
+                f"LFN: {self.is_lfn()}\n")
 
 # Test Case
 if __name__ == "__main__":
-    file = open("./FAT32_simple1.mdf", "rb")
-    file.seek(0x404040) # 0x404040 # 0x400080
-    dir_buffer = file.read(32)
-    dentry = Dentry(dir_buffer)
-    print(dentry)
+     with open("./FAT32_simple1.mdf", "rb") as file:
+        offsets = [0x400080, 0x404040]
+        dir_buffers = [file.read(32) for offset in offsets for _ in [file.seek(offset)]]
+        dentries = [Dentry(buffer) for buffer in dir_buffers]
+        for entry in dentries:
+            print(str(entry))
