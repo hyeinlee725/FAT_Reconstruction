e9516f48083093cc204200ebcae8b36bcc353c04
Hye-In Lee <59090549+hyeinlee725@users.noreply.github.com>
2025-02-03 13:57:14 +0900

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,7 +1,7 @@
 from extent import Extent
 from dentry import Dentry
 from br import BootRecord
-from node import Node
+from node import Node, NodeType
 
 class NodeStream:
     def __init__(self, extents, file):
@@ -9,40 +9,46 @@ class NodeStream:
         self.file = file
         self.current_offset = 0
     
-    def seek(self, offset):
-        self.current_offset = offset
-        self.file.seek(offset)
-    
+    def seek(self, offset, whence=0) -> int:
+        if whence == 0:
+            self.offset = offset
+        elif whence == 1:
+            self.offset += offset
+        elif whence == 2:
+            self.offset -= offset
+        else:
+            raise Exception("wrong argument")
+        return self.offset
+
     def read(self, size):
         data = b''
         remaining = size
         offset = self.current_offset
-        
+
         for extent in self.extents:
             if offset >= extent.start + extent.size:
                 continue
             if remaining <= 0:
                 break
-            
+
             relative_offset = max(0, offset - extent.start)
             self.file.seek(extent.start + relative_offset)
-            
+
             read_size = min(remaining, extent.size - relative_offset)
             data += self.file.read(read_size)
-            
+
             remaining -= read_size
             offset += read_size
 
         self.current_offset = offset
         return data
 
-
 if __name__ == "__main__":
-    offsets = [0x400080, 0x404040]
     with open("./FAT32_simple1.mdf", "rb") as file:
         init_addr = file.read(0x200)
         boot_record = BootRecord(init_addr)
-        
+
+        offsets = [0x400080, 0x404040]
         dentries = []
         extents = []
         for offset in offsets:
@@ -51,13 +57,16 @@ if __name__ == "__main__":
             dentry = Dentry(buffer, boot_record)
             dentries.append(dentry)
             extents.append(Extent(offset, dentry.alloc_size))
-        
+
         node_stream = NodeStream(extents, file)
-        
-        root = Node("root", True)
-        node1 = Node("node_400080", dentries[0].is_dir(), root)
-        node2 = Node("node_404040", dentries[1].is_dir(), root)
-        
-        if not node2.is_dir:
+
+        root = Node("root", None, NodeType.Dir)
+        node1 = Node("node_400080", dentries[0], NodeType.Dir if dentries[0].is_dir() else NodeType.File)
+        node2 = Node("node_404040", dentries[1], NodeType.Dir if dentries[1].is_dir() else NodeType.File)
+
+        root.add_child(node1)
+        root.add_child(node2)
+
+        if not node2.is_dir():
             data_read = node_stream.read(1024)
-            print("Stream data read (1024 bytes):", data_read[:64])
+            print("Stream data read (first 64 bytes):", data_read[:64])
